f73cf65732d3d00471bd5b775190ec3c52b0486d
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-11-08 17:58:21 +0100

    changing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
     <main>
       <div className="hero_bnr">
         <h1>Welcome to my world !</h1>
+        <p>Scroll down to learn more about me (:</p>
       </div>
     </main>
   );
