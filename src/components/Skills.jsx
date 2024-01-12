f89d722fe6e457a1bb9ecc6bc43bc631edc14baf
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-01-12 17:04:54 +0100

    :sparkles: add skills in the section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { skillData } from "../helpers/skill";
+import { Col, Container, Row } from "react-bootstrap";
+import { Element } from "react-scroll";
 
 const Skills = () => {
-  return <section></section>;
+  return (
+    <Element name={"Skills"} id="skills">
+      <section className="section">
+        <Container className="text-center">
+          <h2>Skills</h2>
+          <div className="underline"></div>
+
+          <Row className="mt-3 align-items-center">
+            {skillData.map((skills) => {
+              return (
+                <Col xs={4} key={skills.id} className="my-md-5 ">
+                  <figure>
+                    {skills.skill}
+                    <figcaption>{skills.name}</figcaption>
+                  </figure>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </section>
+    </Element>
+  );
 };
 
 export default Skills;
---
+++ src/helpers/skill.js
@@ -0,0 +1,49 @@
+import { Icon } from "@iconify/react";
+
+export const skillData = [
+  {
+    id: 1,
+    skill: <Icon icon="mdi:language-html5" className="display-4 skill" />,
+    name: "HTML5",
+  },
+  {
+    id: 2,
+    skill: <Icon icon="ion:logo-css3" className="display-4 skill" />,
+    name: "CSS3",
+  },
+  {
+    id: 3,
+    skill: <Icon icon="fa6-brands:js" className="display-4 skill" />,
+    name: "JavaScript",
+  },
+  {
+    id: 5,
+    skill: <Icon icon="mdi:react" className="display-4  skill" />,
+    name: "React",
+  },
+  {
+    id: 7,
+    skill: <Icon icon="akar-icons:redux-fill" className="display-4 skill" />,
+    name: "Redux",
+  },
+  {
+    id: 8,
+    skill: <Icon icon="bi:git" className="display-4 skill" />,
+    name: "Git",
+  },
+  {
+    id: 9,
+    skill: <Icon icon="fa6-brands:square-github" className="display-4 skill" />,
+    name: "GitHub",
+  },
+  {
+    id: 10,
+    skill: <Icon icon="solar:figma-bold" className="display-4 skill" />,
+    name: "Figma",
+  },
+  {
+    id: 11,
+    skill: <Icon icon="file-icons:swagger" className="display-4 skill" />,
+    name: "Swagger editor",
+  },
+];
