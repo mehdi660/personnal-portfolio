c4a32e096a9b60dc681dc1666debcc2603289d5c
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-01-12 16:27:37 +0100

    :lipstick: add scroll icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import scroll from "../assets/scroll-bar.png";
 
 const Hero = () => {
   return (
@@ -6,6 +7,7 @@ const Hero = () => {
       <div className="hero_bnr">
         <h1>Welcome to my world !</h1>
         <p>Scroll down to learn more about me (:</p>
+        <img src={scroll} alt="scroll" width={50} />
       </div>
     </main>
   );
