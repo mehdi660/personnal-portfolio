f215cbe5e1f67b813231b48d16bfa63bc01830f5
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-01-13 16:41:42 +0100

    :memo: project are finnaly all in the project file

--- src/helpers/project.js
+++ src/helpers/project.js
@@ -2,8 +2,10 @@ import mystere from "../assets/mystere.jpg";
 import debug from "../assets/debug.jpg";
 import argent from "../assets/argent.jpg";
 import kasa from "../assets/kasa.jpg";
-// import debug from "../assets/debug.jpg"
-// import debug from "../assets/debug.jpg"
+import seo from "../assets/seo.jpg";
+import food from "../assets/food.jpg";
+import booki from "../assets/booki.jpg";
+import quizz from "../assets/quizz.jpg";
 
 export const nameData = [
   {
@@ -34,39 +36,44 @@ export const nameData = [
   {
     id: 4,
     name: "Kasa",
-    description: "n this project I learned to use React for the first time",
+    description: "In this project, I learned to use React for the first time",
     image: { kasa },
     online: true,
     site: "https://kasa-location-oc.netlify.app",
   },
   {
     id: 5,
-    name: "",
-    description: "Redux",
-    image: "",
+    name: "Nina_Carducci_SEO",
+    description:
+      "In this project, I learned how to optimize the performance of a website, with the lighthouse and wave tools ",
+    image: { seo },
+    online: true,
+    site: "ninna-carducci.netlify.app",
   },
   {
     id: 6,
-    name: "",
-    description: "Git",
-    image: "",
+    name: "OhMyFoodV1",
+    description:
+      "In this project, I learned how to Implement CSS animations and Integrate a mobile-first mockup ",
+    image: { food },
+    online: true,
+    site: "https://ohmyfoood.netlify.app",
   },
   {
     id: 7,
-    name: "",
-    description: "GitHub",
-    image: "",
+    name: "Booki",
+    description:
+      "In this projetc, I learned for the first time to use HTML and CSS",
+    image: { booki },
+    online: true,
+    site: "https://booki-travel-agency.netlify.app",
   },
   {
     id: 8,
-    name: "",
-    description: "Figma",
-    image: "",
-  },
-  {
-    id: 9,
-    name: "",
-    description: "Swagger editor",
-    image: "",
+    name: "Quizz",
+    description: "A little side project where a little quizz game is available",
+    image: { quizz },
+    online: true,
+    site: "https://quizz-site-js.netlify.app/",
   },
 ];
