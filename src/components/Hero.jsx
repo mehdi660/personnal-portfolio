472848d27f6b4aeb4f87b58ffc20b2b4c62b2579
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-01-25 18:10:48 +0100

    :lipstick: text animation in progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+/* eslint-disable jsx-a11y/heading-has-content */
+import React, { useEffect } from "react";
+import { gsap } from "gsap/all";
+import { TextPlugin } from "gsap/all";
+
+gsap.registerPlugin(TextPlugin);
 
 const Hero = () => {
+  useEffect(() => {
+    gsap.to(".split", {
+      stagger: 0.05,
+      delay: 0.1,
+      duration: 3,
+      ease: "power3.inOut", // Use a standard ease for the entire animation
+      text: {
+        value: "Welcome to my world !",
+      },
+    });
+  }, []);
+
   return (
     <main>
       <div className="hero_bnr">
-        <h1>Welcome to my world !</h1>
+        <h1 className="split"></h1>
         <p>Scroll down to learn more about me (:</p>
+
         <div className="scroll">
           <div className="chevron"></div>
           <div className="chevron"></div>
