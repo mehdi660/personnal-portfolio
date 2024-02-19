d1845a080c2aff7f50b82b3d9cf6ffb5e535d96c
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-02-19 14:40:31 +0100

    :hammer: dark mode function in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,12 @@ import Nav from "react-bootstrap/Nav";
 import Navbarr from "react-bootstrap/Navbar";
 import Avatar from "@mui/joy/Avatar";
 import moi from "../assets/moi.jpg";
-import Dark from "./Dark";
 
 function Navbar() {
   return (
     <header>
       <Navbarr data-bs-theme="dark">
         <Container className="d-flex justify-content-center">
-          <Dark />
           <Nav className="me-auto d-flex align-items-center">
             <Avatar src={moi} alt="moi" />
             <Nav.Link className="a_nav" href="#about">
